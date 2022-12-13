a9d4028ba25b01e7eb51ab696a044abe1cd9ed4e
killflex <ferryhasanbackup@gmail.com>
2022-12-13 23:49:08 +0700

    add: project files

---
+++ README.md
@@ -0,0 +1,13 @@
+## Keystore for Releases
+key store password  : EduLaga
+key alias           : edulaga
+key password        : EduLaga
+
+## Keystore Certificate
+Validity of Keystore    : 25 years old
+First and Last Name     : edulaga
+Organizational Unit     : Mobile Game
+Organizational          : Tim edukatif UPNVJT
+City                    : Surabaya
+Province                : East Java
+Country Code            : ID
---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-inappbrowser.inappbrowser",
+      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+      "pluginId": "cordova-plugin-inappbrowser",
+      "clobbers": [
+        "cordova.InAppBrowser.open"
+      ]
+    },
+    {
+      "id": "cordova-plugin-fullscreen.AndroidFullScreen",
+      "file": "plugins/cordova-plugin-fullscreen/www/AndroidFullScreen.js",
+      "pluginId": "cordova-plugin-fullscreen",
+      "clobbers": [
+        "AndroidFullScreen"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-inappbrowser": "5.0.0",
+    "cordova-plugin-fullscreen": "1.3.0",
+    "cordova-plugin-splashscreen": "7.0.0-dev"
+  };
+});
